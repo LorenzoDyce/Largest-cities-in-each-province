b28b5fef4ab343fe0593cb9987695a1c166db515
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-08 14:33:36 -0400

    Populated the alberta component with data

---
+++ src/components/Alberta.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+
+class Alberta extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            albertaData: {
+                labels: ['Calgary', 'Edmonton', 'Red Deer', 'LethBridge', 'Wood Buffalo', 'St Albert'],
+                datasets: [
+                  {
+                    label: 'Population',
+                    data: [ 1200000, 930000, 100000, 93000, 72000, 66000],
+                    backgroundColor: [
+                      'rgba(255, 99, 132, 0.6)',
+                      'rgba(54, 162, 235, 0.6)',
+                      'rgba(255, 206, 86, 0.6)',
+                      'rgba(75, 192, 192, 0.6)',
+                      'rgba(153, 102, 255, 0.6)',
+                      'rgba(255, 159, 64, 0.6)',
+                      'rgba(255, 99, 132, 0.6)'
+                    ],
+                  }
+                ]
+              }    
+        }
+    }
+    render() { 
+        return ( <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: true,
+              text: 'Largest Cities In Alberta',
+              fontSize: 30
+            },
+            legend: {
+              display: false,
+              position: 'bottom',
+              labels: {
+                fontColor: '#000'
+              }
+            },
+            layout: {
+              padding: {
+                left: 90,
+                right: 90,
+                bottom: 40,
+                top: 40
+              }
+            },
+            tooltips: {
+              enabled: true
+            }
+          }}
+        />
+      </div> );
+    }
+}
+ 
+export default Alberta;

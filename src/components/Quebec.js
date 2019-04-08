fbe149cd8282d9fd313eacea734caf997e71d291
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-08 19:49:39 -0400

    initial commit

---
+++ src/components/Quebec.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+
+
+
+
+class Quebec extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quebecData: {
+                labels: ['Montréal', 'Québec', 'Laval', 'Gatineau', 'Longueuil', 'Sherbrooke'],
+                datasets: [
+                  {
+                    label: 'Population',
+                    data: [1782844, 542706, 432858, 282596, 243778, 166863],
+                    backgroundColor: [
+                      'rgba(255, 99, 132, 0.6)',
+                      'rgba(54, 162, 235, 0.6)',
+                      'rgba(255, 206, 86, 0.6)',
+                      'rgba(75, 192, 192, 0.6)',
+                      'rgba(153, 102, 255, 0.6)',
+                      'rgba(255, 159, 64, 0.6)',
+                      'rgba(255, 99, 132, 0.6)'
+                    ],
+                  }
+                ]
+              }
+            }
+          }
+          render() {
+            return (
+              <div className="chart">
+                <Bar
+                  data={this.state.quebecData}
+                  options={{
+                    title: {
+                      display: true,
+                      text: 'Largest Cities In Quebec',
+                      fontSize: 30
+                    },
+                    legend: {
+                      display: false,
+                      position: 'bottom',
+                      labels: {
+                        fontColor: '#000'
+                      }
+                    },
+                    layout: {
+                      padding: {
+                        left: 100,
+                        right: 100,
+                        bottom: 40,
+                        top: 110
+                      }
+                    },
+                    tooltips: {
+                      enabled: true
+                    }
+                  }}
+                />
+              </div>);    
+    }
+}
+ 
+export default Quebec;

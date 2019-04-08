25c3e4670aae52031ee930a9ddbf25a936a7793d
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-08 15:24:20 -0400

    imported Manitoba component to main app component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Alberta from './components/Alberta';
 import BritishColumbia from './components/BritishColumbia';
+import Manitoba from './components/Manitoba'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Alberta />
         <BritishColumbia />
+        <Manitoba/>
       </div>
     );
   }

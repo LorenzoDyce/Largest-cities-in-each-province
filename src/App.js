d142065bcd2748c546bbba918c913f85fc39c31c
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-08 15:12:23 -0400

    imported BritishColumbia component to main app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Alberta from './components/Alberta';
+import BritishColumbia from './components/BritishColumbia';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Alberta />
+        <BritishColumbia />
       </div>
     );
   }

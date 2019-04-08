2f369656fbe165236f4a7465b06c19bb683e3957
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-08 19:42:08 -0400

    imported the react-chartjs-2 package and changed  the padding

--- src/components/NewBrunswick.js
+++ src/components/NewBrunswick.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Bar, Line, Pie } from 'react-chartjs-2';
+import { Bar} from 'react-chartjs-2';
 
 
 
@@ -47,10 +47,10 @@ class NewBrunswick extends Component {
                     },
                     layout: {
                       padding: {
-                        left: 90,
-                        right: 90,
+                        left: 100,
+                        right: 100,
                         bottom: 40,
-                        top: 40
+                        top: 110
                       }
                     },
                     tooltips: {

e4f9675929c315d2d48df02a1afef9e456a4d56d
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-08 19:48:02 -0400

    home initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css'
+
+
+
+class Home extends Component {
+    render() { 
+        return ( <div className="welcome">
+            <h1>WELCOME TO CANADA</h1>
+        </div> );
+    }
+}
+ 
+export default Home;

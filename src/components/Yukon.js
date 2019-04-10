9e7b8ae04bf0dea3f81d2feff9803799053f3832
LorenzoDyce <LorenzoDyce@gmail.com>
2019-04-10 17:06:23 -0400

    Change data colors for Yukon

--- src/components/Yukon.js
+++ src/components/Yukon.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Bar } from 'react-chartjs-2';
+import { Pie } from 'react-chartjs-2';
 
 
 
@@ -19,10 +19,7 @@ class Yukon extends Component {
                       'rgba(255, 99, 132, 0.6)',
                       'rgba(54, 162, 235, 0.6)',
                       'rgba(255, 206, 86, 0.6)',
-                      'rgba(75, 192, 192, 0.6)',
-                      'rgba(153, 102, 255, 0.6)',
-                      'rgba(255, 159, 64, 0.6)',
-                      'rgba(255, 99, 132, 0.6)'
+                      'rgba(75, 192, 192, 0.6)'
                     ],
                   }
                 ]
@@ -32,16 +29,16 @@ class Yukon extends Component {
           render() {
             return (
               <div className="chart">
-                <Bar
+                <Pie
                   data={this.state.yukonData}
                   options={{
                     title: {
                       display: true,
-                      text: 'Largest Cities In Yukon',
+                      text: 'Largest Centres In Yukon',
                       fontSize: 30
                     },
                     legend: {
-                      display: false,
+                      display: true,
                       position: 'bottom',
                       labels: {
                         fontColor: '#000'
